de3ab021e43e22cc9adc3063805ec842473feba0
epmike64 <epmike64@gmail.com>
2024-06-15 14:42:34 -0400

    Next edit

--- src/main/scala/com/fint/tools/util/Graph.scala
+++ src/main/scala/com/fint/tools/util/Graph.scala
@@ -8,9 +8,11 @@ trait NodeData[D] {
 }
 
 class Node[D] private[Node] (val data: NodeData[D]) {
-  val edges = mutable.Map[String, Node[D]]()
+  private val chldEdges = mutable.Map[String, Node[D]]()
+  private val prntEdges = mutable.Map[String, Node[D]]()
 
-  private def connect(node: Node[D]): Unit = {
+  private def conn(node: Node[D], isChld: Boolean): Unit = {
+    val edges = if(isChld) chldEdges else prntEdges
     if(edges.contains(node.data.name)) {
       throw new IllegalArgumentException(s"Node ${node.data.name} already exists in the map")
     }
@@ -20,9 +22,9 @@ class Node[D] private[Node] (val data: NodeData[D]) {
 
 object Node {
   def apply[D](data: NodeData[D]): Node[D] = new Node(data)
-  def connect[D](node1: Node[D], node2: Node[D]): Unit = {
-    node1.connect(node2)
-    node2.connect(node1)
+  def connChld[D](node1: Node[D], node2: Node[D]): Unit = {
+    node1.conn(node2, true)
+    node2.conn(node1, false)
   }
 }
 
@@ -46,7 +48,7 @@ class Graph[D] {
   def connectNodes(node1: String, node2: String): Unit = {
     nodes.get(node1) match {
       case Some(n1) => nodes.get(node2) match {
-        case Some(n2) => Node.connect(n1, n2)
+        case Some(n2) => Node.connChld(n1, n2)
         case None => throw new IllegalArgumentException(s"Node $node2 does not exist in the map")
       }
       case None => throw new IllegalArgumentException(s"Node $node1 does not exist in the map")
