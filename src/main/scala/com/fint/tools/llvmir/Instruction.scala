507a20fddd30b2d5351178d85947835b53ed2049
epmike64 <epmike64@gmail.com>
2024-06-15 17:14:15 -0400

    Next edit

--- src/main/scala/com/fint/tools/llvmir/Instruction.scala
+++ src/main/scala/com/fint/tools/llvmir/Instruction.scala
@@ -1,5 +1,22 @@
 package com.fint.tools.llvmir
 
+enum TermOps(val opcode: Int) {
+  case Ret extends TermOps(1)
+  case Br extends TermOps(2)
+  case Switch extends TermOps(3)
+  case IndirectBr extends TermOps(4)
+  case Invoke extends TermOps(5)
+  case Resume extends TermOps(6)
+  case Unreachable extends TermOps(7)
+  case CleanupRet extends TermOps(8)
+  case CatchRet extends TermOps(9)
+  case CatchSwitch extends TermOps(10)
+  case CallBr extends TermOps(11)
+}
+
+enum UnaryOps(val opcode: Int) {
+  case FNeg extends UnaryOps(12)
+}
 
 enum BinaryOps(val opcode: Int) {
   case Add extends BinaryOps(13)
@@ -22,6 +39,53 @@ enum BinaryOps(val opcode: Int) {
   case Xor extends BinaryOps(30)
 }
 
+enum MemoryOps(val opcode: Int) {
+  case Alloca extends MemoryOps(31)
+  case Load extends MemoryOps(32)
+  case Store extends MemoryOps(33)
+  case GetElementPtr extends MemoryOps(34)
+  case Fence extends MemoryOps(35)
+  case AtomicCmpXchg extends MemoryOps(36)
+  case AtomicRMW extends MemoryOps(37)
+}
+
+enum CastOps(val opcode: Int) {
+  case Trunc extends CastOps(38)
+  case ZExt extends CastOps(39)
+  case SExt extends CastOps(40)
+  case FPToUI extends CastOps(41)
+  case FPToSI extends CastOps(42)
+  case UIToFP extends CastOps(43)
+  case SIToFP extends CastOps(44)
+  case FPTrunc extends CastOps(45)
+  case FPExt extends CastOps(46)
+  case PtrToInt extends CastOps(47)
+  case IntToPtr extends CastOps(48)
+  case BitCast extends CastOps(49)
+  case AddrSpaceCast extends CastOps(50)
+}
+
+enum FuncletPadOps(val opcode: Int) {
+  case CleanupPad extends FuncletPadOps(51)
+  case CatchPad extends FuncletPadOps(52)
+}
+
+enum OtherOps(val opcode: Int) {
+  case ICmp extends OtherOps(53)
+  case FCmp extends OtherOps(54)
+  case PHI extends OtherOps(55)
+  case Call extends OtherOps(56)
+  case Select extends OtherOps(57)
+  case UserOp1 extends OtherOps(58)
+  case UserOp2 extends OtherOps(59)
+  case VAArg extends OtherOps(60)
+  case ExtractElement extends OtherOps(61)
+  case InsertElement extends OtherOps(62)
+  case ShuffleVector extends OtherOps(63)
+  case ExtractValue extends OtherOps(64)
+  case InsertValue extends OtherOps(65)
+  case LandingPad extends OtherOps(66)
+}
 
 class Instruction(val ftype: FType) extends Value(ftype) {
 }
